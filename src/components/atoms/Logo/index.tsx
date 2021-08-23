6b7493a2fcf7610c6c0a4b0fd76ea7ad53c617bf
mono-tmt <tomita@monosus.co.jp>
2021-08-23 15:11:54 +0900

    :sparkles: Create Logo & HumburgerButton

---
+++ src/components/atoms/Logo/index.tsx
@@ -0,0 +1,10 @@
+const Logo = () => {
+  return (
+    <>
+      <a href="#" className="text-xl font-bold text-white md:text-2xl">
+        放送大学
+      </a>
+    </>
+  );
+};
+export default Logo;
--- src/components/templates/Header/index.tsx
+++ src/components/templates/Header/index.tsx
@@ -1,5 +1,8 @@
 import { css } from "linaria";
 import tw from "twin.macro";
+
+import Logo from "../../atoms/Logo";
+import HumburgerButton from "../../atoms/HumburgerButton";
 const Header = () => {
   return (
     <nav
@@ -10,21 +13,8 @@ const Header = () => {
     >
       <div className="container flex flex-col mx-auto md:flex-row md:items-center md:justify-between">
         <div className="flex items-center justify-between">
-          <div>
-            <a href="#" className="text-xl font-bold text-white md:text-2xl">
-              放送大学
-            </a>
-          </div>
-          <div>
-            <button
-              type="button"
-              className="block text-gray-800 hover:text-gray-100 focus:text-gray-600 focus:outline-none md:hidden"
-            >
-              <svg viewBox="0 0 24 24" className="w-6 h-6 fill-current">
-                <path d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
-              </svg>
-            </button>
-          </div>
+          <Logo />
+          <HumburgerButton />
         </div>
         <ul className="flex-col hidden md:flex md:flex-row md:-mx-4">
           <a
