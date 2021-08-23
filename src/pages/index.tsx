d89240fd7b21e1955fae9708abaebcc9f4865899
mono-tmt <tomita@monosus.co.jp>
2021-08-23 16:04:22 +0900

    :art: Create Footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,11 @@
 import React from "react";
-import { css } from "linaria";
-import tw from "twin.macro";
 
-import Button from "../components/atoms/Button";
-
+import { Header, Footer } from "../components/templates";
 export default function Index() {
   return (
-    <div className="App">
-      <h1>Next.js + twin.macro + Linaria</h1>
-      <div
-        className={css`
-          ${tw`mt-[50px] md:bg-gray-500 sm:bg-pink-500 border border-solid border-purple-900 p-6 font-bold`}
-        `}
-      >
-        I am styled with twin.macro + Linaria
-        <Button label="Button" />
-      </div>
-    </div>
+    <>
+      <Header />
+      <Footer />
+    </>
   );
 }
